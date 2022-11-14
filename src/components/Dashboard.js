785b073a2073e0440f25120324f4531c2c46119d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-14 10:54:16 -0800

    upload to github

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,108 @@
 import React, { Component } from "react";
+import axios from "axios";
 
+import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    getValue: getTotalInterviews
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    getValue: getLeastPopularTimeSlot
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    getValue: getMostPopularDay
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    getValue: getInterviewsPerDay
+  }
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+  };
 
-    return <main className={dashboardClasses} />;
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.getValue(this.state)}
+          onSelect={() => this.selectPanel(panel.id)}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
